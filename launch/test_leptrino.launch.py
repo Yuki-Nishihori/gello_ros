6bf4a4359f6b5b656d0e347c03e6390f63cc3997
YusakuNakajima <yusaku_nakajima@ap.eng.osaka-u.ac.jp>
2025-08-31 13:14:18 +0900

    Add test lepterino

--- launch/test_leptrino.launch.py
+++ launch/test_leptrino.launch.py
@@ -2,7 +2,8 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     comport = LaunchConfiguration('comport')
@@ -11,31 +12,52 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'comport',
-            default_value='/dev/serial/by-id/usb-STMicroelectronics_STM32_Virtual_COM_Port_2407028-if00',
-            description='Path to the COM port'),
+            default_value='/dev/ttyACM0',
+            description='Path to the COM port'
+        ),
         DeclareLaunchArgument(
             'sampling_rate',
-            default_value='1200',
-            description='Sampling rate'),
-
-        Node(
-            package='leptrino_force_torque',
-            executable='leptrino_force_torque',
-            name='leptrino',
-            output='screen',
-            parameters=[
-                {
-                    'com_port': comport,
-                    'frame_id': 'leptrino',
-                    'rate': sampling_rate
-                }
-            ]
+            default_value='1200.0',
+            description='Sampling rate'
         ),
-        Node(
-            package='grinding_force_torque',
-            executable='ft_filter.py',
-            name='leptrino_ft_filter',
+
+        ComposableNodeContainer(
+            name='leptrino_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container_mt',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='leptrino_force_torque',
+                    plugin='LeptrinoNode',
+                    name='leptrino_force_torque_node',
+                    namespace='leptrino',
+                    parameters=[{
+                        'com_port': comport,
+                        'frame_id': 'leptrino_frame',
+                        'rate': sampling_rate
+                    }],
+                    extra_arguments=[{'use_intra_process_comms': True}],
+                ),
+                ComposableNode(
+                    package='grinding_force_torque',
+                    plugin='grinding_force_torque::WrenchFilter',
+                    name='leptrino_wrench_filter',
+                    namespace='leptrino',
+                    parameters=[{
+                        'input_topic': '/leptrino/wrench',
+                        'output_topic': '/leptrino/wrench/filtered',
+                        'sampling_frequency': sampling_rate,
+                        'cutoff_frequency': 2.5,
+                        'filter_order': 3,
+                        'data_window': 100,
+                        'initial_zero': True,
+                        'disable_filtering': False
+                    }],
+                    extra_arguments=[{'use_intra_process_comms': True}],
+                )
+            ],
+            parameters=[{'use_intra_process_comms': True}],
             output='screen',
-            arguments=['-z', '-t', 'leptrino/wrench', '-sf', sampling_rate]
         )
     ])
